b40e1e3dae5a182d9a9ca6e5c3c30723dad00a0e
Beatriz Souza <beatrizsouzasantos1288@gmail.com>
2023-10-03 17:56:06 -0300

    Add files via upload
    up

---
+++ I-Am-Ritch/I Am Ritch/I_Am_RitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  I_Am_RitchApp.swift
+//  I Am Ritch
+//
+//  Created by Beatriz on 15/09/23.
+//
+
+import SwiftUI
+
+@main
+struct I_Am_RitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ I-Am-Ritch/README.md
@@ -0,0 +1,2 @@
+# I-Am-Ritch - SwiftUI
+Este projeto foi criado com o objetivo de treinar meus conhecimentos de SwiftUI com o curso Alura disponível em: https://www.udemy.com/course/ios-13-app-development-bootcamp/
